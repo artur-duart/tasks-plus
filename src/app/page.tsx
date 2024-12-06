501f5044c726a48bc4a0b6692db1e1b5c4c71052
Artur Duarte <arturduartemoraes@gmail.com>
2024-12-06 15:17:30 -0300

    feat: start creating the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "@/styles/home.module.css";
+import heroImage from "../../public/assets/hero.svg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Hello, World!</h1>
+    <div className={styles.pageContainer}>
+      <main className={styles.mainContent}>
+        <div className={styles.heroContainer}>
+          <Image
+            className={styles.heroImage}
+            alt="Ilustração de um homem com camiseta azul e uma mulher com camiseta preta, interagindo com elementos gráficos que estão sendo arrastados e soltos em uma tela de computador."
+            src={heroImage}
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+      </main>
     </div>
   );
 }
