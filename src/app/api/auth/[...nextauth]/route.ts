4909d3b4593de364ec3d3362ff6c39068d48594d
Artur Duarte <arturduartemoraes@gmail.com>
2024-12-08 23:25:08 -0300

    fix: fix authentication route configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const handler = NextAuth({
+const authHandler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -11,4 +11,5 @@ export const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
 });
 
-export { handler as GET, handler as POST };
+export const GET = authHandler;
+export const POST = authHandler;
