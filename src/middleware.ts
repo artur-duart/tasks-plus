e045b241dad77e5c29118e1d48bd88ccee084477
Artur Duarte <arturduartemoraes@gmail.com>
2024-12-10 12:50:48 -0300

    feat: protect routes against unauthorized access

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
