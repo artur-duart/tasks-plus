def804733abe53599ee6e69bffceb54e4363fbc4
Artur Duarte <arturduartemoraes@gmail.com>
2024-12-15 02:42:01 -0300

    feat: register tasks in the database

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,70 +1,108 @@
-import type { Metadata } from "next";
+"use client";
+
 import styles from "./dashboard.module.css";
 import { Textarea } from "@/components/textarea/Textarea";
 import { FaShare, FaTrash } from "react-icons/fa";
-
-export const metadata: Metadata = {
-  title: "Dashboard",
-};
+import { ChangeEvent, FormEvent, useState } from "react";
+import { db } from "@/services/fireBaseConnectiont";
+import { addDoc, collection } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
+
+  const { data: session } = useSession();
+
+  async function handleRegisterTask(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    const user = session?.user;
+
+    try {
+      await addDoc(collection(db, "tasks"), {
+        task: input,
+        created: new Date(),
+        user: user?.email,
+        public: publicTask,
+      });
+
+      setInput("");
+      setPublicTask(false);
+    } catch (err) {
+      console.log("Erro ao registrar tarefa:", err);
+    }
+  }
+
   return (
-    <div className={styles.pageContainer}>
-      <main className={styles.mainContent}>
-        <section className={styles.sectionForm}>
-          <h1 className={styles.title}>Qual sua tarefa?</h1>
+    <>
+      <div className={styles.pageContainer}>
+        <main className={styles.mainContent}>
+          <section className={styles.sectionForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
 
-          <form action="">
-            <Textarea
-              name="nomeTarefa"
-              id="nomeTarefa"
-              placeholder="Digite sua terafa..."
-            />
-
-            <div className={styles.checkboxArea}>
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className={styles.checkbox}
+            <form onSubmit={handleRegisterTask}>
+              <Textarea
+                name="nomeTarefa"
+                id="nomeTarefa"
+                placeholder="Digite sua tarefa..."
+                value={input}
+                onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                  setInput(e.target.value)
+                }
               />
-              <label htmlFor="">Tornar tarefa pública?</label>
-            </div>
 
-            <button type="submit" className={styles.button}>
-              Registrar
-            </button>
-          </form>
-        </section>
+              <div className={styles.checkboxArea}>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  id="checkbox"
+                  className={styles.checkbox}
+                  checked={publicTask}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setPublicTask(e.target.checked)
+                  }
+                />
+                <label htmlFor="checkbox">Tornar tarefa pública?</label>
+              </div>
 
-        <hr className={styles.divisor} />
-
-        <section className={styles.sectionTask}>
-          <h1>Minhas tarefas</h1>
-
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PÚBLICO</label>
-              <button className={styles.shareButton}>
-                <FaShare size={20} color="#0866ff" />
+              <button type="submit" className={styles.button}>
+                Registrar
               </button>
-            </div>
+            </form>
+          </section>
 
-            <div className={styles.taskContent}>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-                architecto repudiandae exercitationem, incidunt a ut, sint
-                recusandae, nemo temporibus quam quaerat magni officia eius
-                vitae. Modi suscipit quam rerum delectus!
-              </p>
+          <hr className={styles.divisor} />
 
-              <button className={styles.trashButton}>
-                <FaTrash size={20} color="#ff4d4f" />
-              </button>
-            </div>
-          </article>
-        </section>
-      </main>
-    </div>
+          <section className={styles.sectionTask}>
+            <h1>Minhas tarefas</h1>
+
+            <article className={styles.task}>
+              <div className={styles.tagContainer}>
+                <label className={styles.tag}>PÚBLICO</label>
+                <button className={styles.shareButton}>
+                  <FaShare size={20} color="#0866ff" />
+                </button>
+              </div>
+
+              <div className={styles.taskContent}>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                  architecto repudiandae exercitationem, incidunt a ut, sint
+                  recusandae, nemo temporibus quam quaerat magni officia eius
+                  vitae. Modi suscipit quam rerum delectus!
+                </p>
+
+                <button className={styles.trashButton}>
+                  <FaTrash size={20} color="#ff4d4f" />
+                </button>
+              </div>
+            </article>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
